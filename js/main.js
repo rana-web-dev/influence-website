c8feab3e10fd9b3ecdbc52255d65d6a1832b50a6
Jahiruddin Rana <jahiruddinrana57@gmail.com>
2022-07-20 17:38:58 +0600

    update nav toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 function menu() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('navbar-ml');
-    
-    nav.classList.toggle('navbar-ml');
+}
+function navClose() {
+  const removeClass = document.getElementById("nav");
+  removeClass.classList.remove("navbar-ml");
 }
